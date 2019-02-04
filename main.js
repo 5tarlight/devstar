9fc2aef6bba8b4daf13be2df9005fcc1a7148341
5tarlight <developerstarlight@gmail.com>
2019-02-04 19:10:31 +0900

    openable server

--- main.js
+++ main.js
@@ -12,16 +12,37 @@ const cors = require('cors')
 const expressSession = require('express-session')
 
 const app = express()
+const router = express.Router()
 
 app.set('port', process.env.PORT || 80)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
-app.use('/public', serveStatic(path.join(__dirname), 'public'))
+app.use('/public', serveStatic(path.join(__dirname, 'public')))
 app.use(cookieParser())
 app.use(expressSession({
   secret: 'my key',
   resave: true,
   saveUninitialized: true
 }))
+
+router.route('/').get((req, res) => {
+  console.log(req.ip + ' : portal')
+  res.end("Hello World")
+})
+
+app.use('/', router)
+
+const errorhandler = expressErrorHandler({
+  static: {
+    '404': './public/404.html'
+  }
+})
+
+app.use(expressErrorHandler.httpError(404))
+app.use(errorhandler)
+
+http.createServer(app).listen(app.get('port'), () => {
+  console.log('Server on : ' + app.get('port'))
+})
